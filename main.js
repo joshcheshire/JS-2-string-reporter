471932f1e429cee17c007b30dbf51a2a70aa28da
Student <student@RU-017.local>
2015-09-07 16:18:12 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var input= prompt("Enter any word!")
+
+var name= 'snake'
+
+var info= " you entered " + name +	
+	"\n There are " + name.length + " characters in your word.";
+	"\n The third letter is " + name.charAt(2)
+	"\n Lowercase " + name.toLowerCase() +
+	"\n Uppercase " + name.toUpperCase() +
+	"\n I have wanted a " + name + " since I was a little boy"
+
+alert(info)
+
