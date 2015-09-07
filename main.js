7819b5ec4ec474b6ad4fb8c05aae9bddd0cea3b7
Student <student@RU-017.local>
2015-09-07 16:26:02 -0600

    completed

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ var input= prompt("Enter any word!")
 var name= 'snake'
 
 var info= " you entered " + name +	
-	"\n There are " + name.length + " characters in your word.";
-	"\n The third letter is " + name.charAt(2)
+	"\n There are " + name.length + " characters in your word." +
+	"\n The third letter is " + name.charAt(2) + 
 	"\n Lowercase " + name.toLowerCase() +
 	"\n Uppercase " + name.toUpperCase() +
-	"\n I have wanted a " + name + " since I was a little boy"
+	"\n I have wanted a " + name + " since I was a little boy" +
+	"\n Subword " + name.substring(1,4)
 
 alert(info)
 
